08b9fc88524e61ff18f248bba0e6c3533d9d3fa4
Estudiante Escuela Politecnica Superior <alejandro1benimeli@gmail.com>
2021-03-09 00:25:59 +0100

    PPU operation skeleton

--- src/ppu.c
+++ src/ppu.c
@@ -13,6 +13,13 @@
 #define LCDC_OBJ_ENABLE         0x02
 #define LCDC_BG_WIN_PRIORITY    0x01
 
+#define STAT_COINC_INT          0x40
+#define STAT_OAM_INT            0x20
+#define STAT_VBLANK_INT         0x10
+#define STAT_HBLANK_INT         0x08
+#define STAT_COINC_FLAG         0x04
+#define STAT_MODE_FLAG          0x03
+
 
 // Color palette for the GB screen
 static uint32_t colors[4] = {0xFFFFFFFF,0xFFA9A9A9,0xFF696969,0xFF000000};
@@ -53,5 +60,127 @@ void free_ppu(PPU *ppu) {
 }
 
 void tick_ppu(GB *gb) {
+
+    uint8_t stat_cond_this_tick = 0; // Will manage this later
+
+
+    if (gb->ppu.ly == gb->ppu.lyc) {
+        gb->ppu.stat |= STAT_COINC_FLAG;   // Sets the LY=LYC coincidence flag
+        if (gb->ppu.stat & STAT_COINC_INT) {  // If LY=LYC stat interrupt is enabled
+            stat_cond_this_tick = 1;
+        }
+    } else { // Clears the flag if LY!=LYC
+        gb->ppu.stat &= 0xFB;
+    }
+
+    switch (gb->ppu.state) {
+
+        case OAM_SEARCH:
+
+            if (gb->ppu.stat & STAT_OAM_INT) {
+                stat_cond_this_tick = 1;
+            }
+
+            if (gb->ppu.ticks == 0) {
+
+                // TODO
+
+            } else if (gb->ppu.ticks == 79) {
+                gb->ppu.state = DATA_TRANSFER;
+                gb->ppu.stat = (gb->ppu.stat & 0xFC) | DATA_TRANSFER;
+            }
+
+            ++gb->ppu.ticks;
+
+            break;
+
+        case OAM_BUG:
+
+            if (gb->ppu.stat & STAT_OAM_INT) {
+                stat_cond_this_tick = 1;
+            }
+
+            if (gb->ppu.ticks == 0) {
+
+                // TODO
+
+            } else if (gb->ppu.ticks == 75) {
+                gb->ppu.state = DATA_TRANSFER;
+                gb->ppu.stat = (gb->ppu.stat & 0xFC) | DATA_TRANSFER;
+                gb->ppu.ticks += 4;
+            }
+
+            ++gb->ppu.ticks;
+
+            break;
+
+        case DATA_TRANSFER:
+
+            // TODO
+
+            ++gb->ppu.curr_pxl;
+            if (gb->ppu.curr_pxl == WIDTH) {
+                gb->ppu.curr_pxl = 0;
+                gb->ppu.state = HBLANK;
+                gb->ppu.stat = (gb->ppu.stat & 0xFC) | HBLANK;
+            }
+
+            ++gb->ppu.ticks;
+
+            break;
+
+        case HBLANK:
+
+            if (gb->ppu.stat & STAT_HBLANK_INT) {
+                stat_cond_this_tick = 1;
+            }
+
+            if (gb->ppu.ticks == 455) {
+                ++gb->ppu.ly;
+                gb->ppu.ticks = 0;
+                if (gb->ppu.ly == HEIGHT) {
+                    gb->ppu.state = VBLANK;
+                    gb->ppu.stat = (gb->ppu.stat & 0xFC) | VBLANK;
+
+                    // Reset line counter for the window
+                    gb->ppu.window_line_counter = 0;
+
+                    v_blank_interrupt(gb);
+
+                } else {
+                    gb->ppu.state = OAM_SEARCH;
+                    gb->ppu.stat = (gb->ppu.stat & 0xFC) | OAM_SEARCH;
+                }   
+            } else {
+                ++gb->ppu.ticks;
+            }
+
+            break;
+
+        case VBLANK:
+
+            if (gb->ppu.stat & STAT_VBLANK_INT) {
+                stat_cond_this_tick = 1;
+            }
+
+            if (gb->ppu.ticks == 455) {
+                ++gb->ppu.ly;
+                gb->ppu.ticks = 0;
+                if (gb->ppu.ly == 154) {
+                    gb->ppu.ly = 0;
+                    gb->ppu.state = OAM_SEARCH;
+                    gb->ppu.stat = (gb->ppu.stat & 0xFC) | OAM_SEARCH;
+                }   
+            } else {
+                ++gb->ppu.ticks;
+            }
+            break;
+
+    }
+
+    if (!gb->ppu.stat_cond_met && stat_cond_this_tick) {
+        stat_interrupt(gb);
+    }
+    gb->ppu.stat_cond_met = stat_cond_this_tick;
     
 }
